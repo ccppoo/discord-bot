9c1e2b679f83fceae863a139c118a80a1b213b3f
ccppoo <shmishmi79@gmail.com>
2022-01-17 14:57:47 +0900

    docker and sample code

---
+++ Dockerfile
@@ -0,0 +1,47 @@
+FROM python:3.8-slim
+
+# install git for installing dependencies via pip <repository>
+RUN apt-get update && \
+    apt-get upgrade -y && \
+    apt-get install -y git
+
+# add user (change to whatever you want)
+# prevents running sudo commands
+RUN useradd -r -s /bin/bash alex
+
+# set current env
+ENV HOME /app
+WORKDIR /app
+ENV PATH="/app/.local/bin:${PATH}"
+
+RUN chown -R alex:alex /app
+USER alex
+
+# set argument vars in docker-run command
+ARG AWS_ACCESS_KEY_ID
+ARG AWS_SECRET_ACCESS_KEY
+ARG AWS_DEFAULT_REGION
+
+# ENV AWS agrument
+ENV AWS_ACCESS_KEY_ID $AWS_ACCESS_KEY_ID
+ENV AWS_SECRET_ACCESS_KEY $AWS_SECRET_ACCESS_KEY
+ENV AWS_DEFAULT_REGION $AWS_DEFAULT_REGION
+
+# set Discord APP token argument
+ARG DISCORD_BOT_TOKEN
+
+# ENV discord argument
+ENV DISCORD_BOT_TOKEN $DISCORD_BOT_TOKEN
+
+# Avoid cache purge by adding requirements first
+ADD ./requirements.txt ./requirements.txt
+
+RUN pip install --upgrade pip --user
+RUN pip install --no-cache-dir -r ./requirements.txt --user
+
+# Add the rest of the files
+COPY . /app
+WORKDIR /app
+
+# start discord bot
+CMD ["python", "app.py"]
---
+++ app.py
@@ -0,0 +1,108 @@
+# 이 예시는 Discord Development Portals에서 'members' 권한 인텐트를 클릭해야 사용할 수 있습니다!
+import random
+
+import discord
+import asyncio
+from discord.message import Message
+from discord.ext import commands
+from discord.ext.commands.context import Context
+from discord.ui import Button, View
+from typing import List
+import os
+
+TOKEN = os.environ['DISCORD_BOT_TOKEN']
+
+# # 코드가 너무 길어지면 스크립트를 따로 만들어서 import 할 수 있습니다.
+# from library import 도서관
+
+# # slash command import
+# from slash_commands.library import library_command
+
+description = """Discord Bot 샘플 봇입니다"""
+
+intents = discord.Intents.default()
+intents.members = True
+
+bot = commands.Bot(command_prefix="!", description=description, intents=intents)
+
+
+# library.py 에서 import 한 명령어
+# bot.add_command(도서관)
+# bot.add_application_command(library_command)
+
+@bot.event
+async def on_ready():
+    print(f"로그인 성공! 닉네임: {bot.user} 아이디(ID): {bot.user.id}")
+    print("------")
+
+@bot.event
+async def on_message(message : Message):
+    # 이 봇이 작성한 메세지는 무시
+    if message.author.id == bot.user.id:
+        return
+    
+    # 다른 봇이 보낸 메시지의 경우 무시
+    if message.author.bot:
+        return
+    
+    # 여러분이 만든 서버(길드)만 메세지를 받고 싶다면 이 조건문을 이용하세요
+    # 봇을 초대한 서버(길드)이름을 알고 싶다면 print(message.guild.name)를 통해서 확인!
+    # if not message.guild.name == "Bot Test":
+    #     return
+    
+    await message.reply(f"{message.author.nick} sent : {message.content}")
+    
+    # 사용자가 보낸 메세지 객체 - 길드 이름, 유저 이름, 아이디, ... 많은 정보를 가지고 있습니다.
+    print(message)
+    # 보낸 메세지
+    print(message.content)
+    
+    # 위에 명시한 command_prefix(명령어 접두어)가 포함된 메세지의 경우
+    # 명령을 수행하기 위해 이 코드는 on_message 함수 맨 아래에 작성합니다!
+    await bot.process_commands(message)
+
+
+@bot.command(description="안녕!")
+async def hello(ctx : Context):
+    button = Button(label="Click me!", style=discord.ButtonStyle.green, emoji="😂")
+    view = View()
+    view.add_item(button)
+    await ctx.send("Hi!", view=view)
+
+
+@bot.command(description="랜덤으로 골라주기 사용법: !랜덤 하나 둘 셋")
+async def 랜덤(ctx : Context, *choices: List[str]):
+    """
+        choices : List[str]
+            사용자가 보낸 명령어를 제외한 메세지
+            (위 예시의 경우 ["하나", "둘", "셋"])
+            으로 받습니다.
+    """
+    await ctx.send(random.choice(choices))
+
+@bot.command(description="주어진 초 이후에 알람을 보내드립니다!")
+async def 알람(ctx : Context, seconds : str = None):
+    """
+        seconds : int
+            주어진 자연수 만큼 대기 이후 @mention을 통해 알림을 보냅니다.
+    """
+    if seconds is None:
+        await ctx.send(f"몇 초 뒤에 알람을 드릴지 알려주세요!\n사용 예시: !알림 5")
+    
+    try:
+        # 사용자가 입력한 메세지는 str(문자열) 타입으로 들어오기 때문에
+        # int(정수형)으로 변환 후 작업을 수행한다.
+        seconds = int(seconds)
+
+        await asyncio.sleep(seconds)
+        await ctx.send(f"{ctx.author.mention}님 {seconds}초가 지났어요!")
+    except Exception as e:
+        print(e)
+        await ctx.send(f"이건 자연수가 아니자나! {seconds} ")
+
+@bot.command()
+async def joined(ctx : Context, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f"{member.name} joined in {member.joined_at}")
+
+bot.run(TOKEN)
