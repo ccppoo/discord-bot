014c213fde084b104588928a158c255727233831
ccppoo <shmishmi79@gmail.com>
2022-02-03 18:54:06 +0900

    Update SampleCog.py

--- src/commands/SampleCog.py
+++ src/commands/SampleCog.py
@@ -7,22 +7,30 @@ class SampleCog(commands.Cog):
     
     def __init__(self, bot) -> None:
         self.bot : commands.Bot = bot
-        
-    # @commands.Cog.listener("on_connect")
-    # async def load_database(self):
-    #     pass
     
     @commands.Cog.listener("on_ready")
     async def I_am_ready(self):
         print("SampleCog(commands.Cog) ready")
     
-    # @commands.Cog.listener("on_message")
-    # async def log_messages(self, message : Message):
-    #     pass
+    @commands.Cog.listener("on_message")
+    async def mirror(self, message : Message):
+        pass
     
-    @commands.command(name="hello", alias=['안녕'])
+    @commands.command(name="ping", aliases= ["핑"])
+    async def ping(self, ctx : Context):
+        await ctx.reply(f"Pong! {self.bot.latency:.2f} ms")
+
+    @ping.error
+    async def on_error(self, ctx : Context, exception : Exception):
+        await ctx.reply(f"이런! Ping 할 수 없군요! : `{exception}`")
+
+    @commands.command(name="hello", aliases=['안녕'])
     async def greetings(self, ctx : Context):
         await ctx.reply(f"{ctx.author.nick} 안녕!")
-        
+    
+    @greetings.error
+    async def on_error(self, ctx : Context, exception : Exception):
+        await ctx.reply(f"이런! 안녕 할 수 없군요! : `{exception}`")
+    
 def setup(bot : commands.Bot):
     bot.add_cog(SampleCog(bot))
